1f4118b0d29b32e82f75ada1fa2f11208e636b0e
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-03-07 16:31:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
 
     # Get a bot token from botfather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6365859811:AAF1Aj_VrbdxS9aPED2PqjwRaeEi4fcm_JE")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6803879488:AAFZAryvguDSx9DCXUXfeHSIbwL2b9HjOu0")
 
 
     # Get from my.telegram.org (or @UseTGXBot)
